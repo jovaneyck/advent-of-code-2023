07843e681ab9557855f4d69ac01a8ef80d1bf32f
Jo Van Eyck <j.vaneyck@farmad.be>
2023-11-26 15:41:04 +0100

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advent-of-code-2023
-F# solutions to the 2023 advent of code puzzles
+
+F# solutions to the [advent of code 2023](https://adventofcode.com/2023)🎄 puzzles.
+
+Watch me struggle on these puzzles live on 📺[twitch](https://www.twitch.tv/prgmtc) or 📺[youtube](https://www.youtube.com/@JoVanEyck/videos)
---
+++ day 00/part1.fsx
@@ -0,0 +1,18 @@
+#r "nuget: Unquote"
+open Swensen.Unquote
+
+let input =
+    System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}\input.txt"""
+    |> List.ofSeq
+
+let example =
+    """hehe""".Split("\n")
+    |> Array.map (fun s -> s.Trim())
+    |> List.ofSeq
+
+let run () =
+    printf "Testing.."
+    test <@ 1 + 1 = 2 @>
+    printfn "...done!"
+
+run ()
