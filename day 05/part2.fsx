514ae8b0358f0b76bf87059b3f35d921467c82ff
Jo Van Eyck <j.vaneyck@farmad.be>
2023-12-07 21:29:41 +0100

    Decided to just brute force 5.2 after all. Works but took a while :)

--- day 05/part2.fsx
+++ day 05/part2.fsx
@@ -1,99 +1,61 @@
-//Part 2 IDEA: instead of transforming seed ids
-//Let's lift the problem and transform entire RANGES instead
-
 #r "nuget: Unquote"
 open Swensen.Unquote
 
-//let input =
-//    System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}\input.txt"""
-//    |> List.ofSeq
-
-let example =
-    """seeds: 79 14 55 13
-
-    seed-to-soil map:
-    50 98 2
-    52 50 48
-
-    soil-to-fertilizer map:
-    0 15 37
-    37 52 2
-    39 0 15
-
-    fertilizer-to-water map:
-    49 53 8
-    0 11 42
-    42 0 7
-    57 7 4
-
-    water-to-light map:
-    88 18 7
-    18 25 70
-
-    light-to-temperature map:
-    45 77 23
-    81 45 19
-    68 64 13
-
-    temperature-to-humidity map:
-    0 69 1
-    1 0 69
-
-    humidity-to-location map:
-    60 56 37
-    56 93 4"""
-        .Split("\n")
-    |> Array.map (fun s -> s.Trim())
+let input =
+    System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}\input.txt"""
     |> List.ofSeq
 
-/// <summary>
-/// Represents a head-inclusive tail-exclusive range
-/// </summary>
-type Range =
-    { Start: int64
-      Length: int64 }
+//let example =
+//    """seeds: 79 14 55 13
 
-    member this.End = this.Start + this.Length
+//    seed-to-soil map:
+//    50 98 2
+//    52 50 48
 
-/// <summary>
-/// Represents a head-inclusive tail-exclusive range
-/// </summary>
-module Range =
-    let create start ending =
-        { Start = start
-          Length = ending - start }
+//    soil-to-fertilizer map:
+//    0 15 37
+//    37 52 2
+//    39 0 15
 
-    let createStartLength start length = { Start = start; Length = length }
+//    fertilizer-to-water map:
+//    49 53 8
+//    0 11 42
+//    42 0 7
+//    57 7 4
 
-    let contains number range =
-        range.Start <= number && number < range.End
+//    water-to-light map:
+//    88 18 7
+//    18 25 70
 
-    let isEmpty range = range.Length < 0
+//    light-to-temperature map:
+//    45 77 23
+//    81 45 19
+//    68 64 13
 
-    let overlap (other: Range) (one: Range) =
-        let start = max one.Start other.Start
-        let ending = min one.End other.End
-        create start ending
+//    temperature-to-humidity map:
+//    0 69 1
+//    1 0 69
 
-    /// <summary>
-    /// splits a range at elements in the range (not an index).
-    /// </summary>
-    let split positions (range : Range) : Range list = [ range ]
+//    humidity-to-location map:
+//    60 56 37
+//    56 93 4"""
+//        .Split("\n")
+//    |> Array.map (fun s -> s.Trim())
+//    |> List.ofSeq
 
-    /// <summary>
-    /// Slices one original range given other, resulting in 1..3 subranges of the original depending on the overlaps with other.
-    /// </summary>
-    let slice (other: Range) (original: Range) : Range list = 
-        original |> split [other.Start; other.End]
+type Entry =
+    { SourceStart: uint64
+      DestinationStart: uint64
+      RangeLength: uint64 }
 
-type Entry = { Range: Range; Offset: int64 }
 type Map = Entry list
 
 let parseEntry (entry: string) =
-    let [| dest; src; length |] = entry.Split(" ") |> Array.map int64
+    let [| dest; src; length |] = entry.Split(" ") |> Array.map uint64
 
-    { Range = Range.createStartLength src length
-      Offset = dest - src }
+    { SourceStart = src
+      DestinationStart = dest
+      RangeLength = length }
 
 let rec chunkBy sep lines =
     if lines |> Seq.isEmpty then
@@ -103,110 +65,105 @@ let rec chunkBy sep lines =
         let rest = lines |> List.skip (nextChunk.Length + 1)
         nextChunk :: (chunkBy sep rest)
 
+let chunks = chunkBy "" input
+
+let seedToSoil: Map = chunks[1] |> List.skip 1 |> List.map parseEntry
+let soilToFertilizer: Map = chunks[2] |> List.skip 1 |> List.map parseEntry
+let fertilizerToWater: Map = chunks[3] |> List.skip 1 |> List.map parseEntry
+let waterToLight: Map = chunks[4] |> List.skip 1 |> List.map parseEntry
+let lightToTemperature: Map = chunks[5] |> List.skip 1 |> List.map parseEntry
+let temperatureToHumidity: Map = chunks[6] |> List.skip 1 |> List.map parseEntry
+let humidityToLocation: Map = chunks[7] |> List.skip 1 |> List.map parseEntry
+
+let inSourceRange number (entry: Entry) =
+    entry.SourceStart <= number
+    && number <= entry.SourceStart + entry.RangeLength
+
+let corresponding number (entry: Entry) =
+    let offset = number - entry.SourceStart
+    entry.DestinationStart + offset
+
+let lookup (map: Map) number =
+    let entry = map |> List.tryFind (inSourceRange number)
+
+    match entry with
+    | None -> number
+    | Some entry -> (corresponding number entry)
+
+let debug msg x =
+    //printfn msg
+    //printfn "%A" x
+    x
+
+let lookupchain =
+    debug "\n let's go"
+    >> (lookup seedToSoil)
+    >> debug "seed to soil"
+    >> (lookup soilToFertilizer)
+    >> debug "soil to fert"
+    >> (lookup fertilizerToWater)
+    >> debug "fert to water"
+    >> (lookup waterToLight)
+    >> debug "water to light"
+    >> (lookup lightToTemperature)
+    >> debug "light to temp"
+    >> (lookup temperatureToHumidity)
+    >> debug "temp to hum"
+    >> (lookup humidityToLocation)
+    >> debug "humidity to loc"
+
+
+
 let seeds =
-    (example[ 0 ].Split("seeds: ")[1]).Split(" ")
-    |> Array.chunkBySize 2
-    |> Array.map (fun [| start; length |] ->
-        { Start = int64 start
-          Length = int64 length })
-    |> Seq.toList
+    (input[ 0 ].Split("seeds: ")[1]).Split(" ")
+    |> Seq.map uint64
+    |> Seq.chunkBySize 2
+    |> Seq.collect (fun [| start; length |] -> [ start .. (start + length - 1UL) ])
 
-let chunks = chunkBy "" example
-
-let seedToSoil: Map =
-    chunks[1]
-    |> List.skip 1
-    |> List.map parseEntry
-    |> List.sortBy (fun e -> e.Range.Start)
-
-let soilToFertilizer: Map =
-    chunks[2]
-    |> List.skip 1
-    |> List.map parseEntry
-    |> List.sortBy (fun e -> e.Range.Start)
-
-let fertilizerToWater: Map =
-    chunks[3]
-    |> List.skip 1
-    |> List.map parseEntry
-    |> List.sortBy (fun e -> e.Range.Start)
-
-let waterToLight: Map =
-    chunks[4]
-    |> List.skip 1
-    |> List.map parseEntry
-    |> List.sortBy (fun e -> e.Range.Start)
-
-let lightToTemperature: Map =
-    chunks[5]
-    |> List.skip 1
-    |> List.map parseEntry
-    |> List.sortBy (fun e -> e.Range.Start)
-
-let temperatureToHumidity: Map =
-    chunks[6]
-    |> List.skip 1
-    |> List.map parseEntry
-    |> List.sortBy (fun e -> e.Range.Start)
-
-let humidityToLocation: Map =
-    chunks[7]
-    |> List.skip 1
-    |> List.map parseEntry
-    |> List.sortBy (fun e -> e.Range.Start)
-
-let rec transform (map: Map) (entry: Entry) : Map =
-    if entry.Range |> Range.isEmpty then
-        [] //Fully transformed the range
-    else
-        match map with
-        | [] -> [ entry ] //no more entries, identity
-        | h :: t ->
-            let overlap = Range.overlap entry.Range h.Range
-
-            if overlap |> Range.isEmpty then
-                transform t entry //no overlap, proceed to next entry
-            else if entry.Range = overlap then //exact match, apply offset and stop
-                [ { Range = overlap
-                    Offset = entry.Offset + h.Offset } ]
-            else
-                []
-
-//let transformed = seeds |> List.collect (transform seedToSoil)
-//let seedToLocation seeds =
-//    [ seedToSoil
-//      soilToFertilizer
-//      fertilizerToWater
-//      waterToLight
-//      lightToTemperature
-//      temperatureToHumidity
-//      humidityToLocation ]
-//    |> List.fold (fun x map -> transform map x) seeds
-
-//let transformed = [ seeds ] |> List.collect seedToLocation
-
+let soils = seeds |> Seq.map lookupchain
 #time
-
+let solution = soils |> Seq.min
+//Real: 00:15:46.551, CPU: 00:29:51.812, GC gen0: 13586, gen1: 15, gen2: 8
+//72263011UL
 let run () =
     printf "Testing.."
     test <@ chunkBy "" [ ""; "a"; "b"; ""; "c"; "d" ] = [ [ "a"; "b" ]; [ "c"; "d" ] ] @>
 
-    test <@ { Start = 8L; Length = 3L } |> Range.contains 10L @>
+    test
+        <@ inSourceRange
+            10UL
+            { SourceStart = 8UL
+              RangeLength = 2UL
+              DestinationStart = 1UL } @>
 
     test
-        <@ { Start = 8L; Length = 2L }
-           |> Range.contains 10L
-           |> not @>
+        <@ inSourceRange
+            10UL
 
-    test <@ { Start = 10L; Length = 2L } |> Range.contains 10L @>
+            { SourceStart = 10UL
+              RangeLength = 2UL
+              DestinationStart = 1UL } @>
 
     test
-        <@ { Start = 11L; Length = 2L }
-           |> Range.contains 10L
+        <@ inSourceRange
+            10UL
+
+            { SourceStart = 7UL
+              RangeLength = 2UL
+              DestinationStart = 1UL }
            |> not @>
 
-    test <@ Range.createStartLength 1 2 |> _.End = 3 @>
-    test <@ Range.create 1 3 |> _.Length = 2 @>
+    test
+        <@ inSourceRange
+            10UL
+
+            { SourceStart = 11UL
+              RangeLength = 2UL
+              DestinationStart = 1UL }
+           |> not @>
+
+    test <@ lookup seedToSoil 53UL = 55UL @>
+    test <@ lookup seedToSoil 1337UL = 1337UL @>
     printfn "...done!"
 
 run ()
