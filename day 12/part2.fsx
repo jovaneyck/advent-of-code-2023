f4c05c4cd9dda276167cea84f8b0c11089e4fada
Jo Van Eyck <j.vaneyck@farmad.be>
2023-12-29 19:36:20 +0100

    12.2 rewrite

--- day 12/part2.fsx
+++ day 12/part2.fsx
@@ -1,7 +1,6 @@
 #r "nuget: Unquote"
 
 open Swensen.Unquote
-open System.Collections.Generic
 
 let input =
     System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}\input.txt"""
@@ -37,88 +36,6 @@ let parse (line: string) =
 
     (parsedChars, parsedGroupSizes)
 
-let rec generateCombinations chars =
-    match chars with
-    | [] -> [ [] ]
-    | Empty :: cs ->
-        (generateCombinations cs
-         |> List.map (fun c -> Empty :: c))
-    | Spring :: cs ->
-        (generateCombinations cs
-         |> List.map (fun c -> Spring :: c))
-    | Unknown :: cs ->
-        let replacedByEmpty = generateCombinations (Empty :: cs)
-        let replacedBySpring = generateCombinations (Spring :: cs)
-        replacedByEmpty @ replacedBySpring
-
-//MUTABLE dictionary, immutable alternatives exist but are a hassle
-//to thread through recursive functions
-//TODO: investigate whether a state monad could help here?
-type Memo = Dictionary<int list * Char list, bool>
-
-let rec validCombination (memo: Memo) sizes chars =
-    let key = (sizes, chars)
-
-    match memo.TryGetValue(key) with
-    | (true, result) -> result
-    | (false, _) ->
-        let result =
-            if (sizes |> List.sum) > (chars |> Seq.length) then
-                false
-            else
-                match sizes with
-                | [] -> chars |> List.forall ((=) Empty)
-                | s :: ss ->
-                    match chars with
-                    | [] -> false
-                    | Unknown :: _ -> failwith "Did not expect an unkown character in here."
-                    | Empty :: cs -> validCombination memo sizes cs
-                    | Spring :: cs ->
-                        if cs |> List.length < (s - 1) then
-                            false
-                        else
-                            let otherSprings = cs |> List.take (s - 1)
-                            let areAllSprings = (otherSprings |> List.forall ((=) Spring))
-
-                            if areAllSprings |> not then
-                                false
-                            else
-                                let remainder = cs |> List.skip (s - 1)
-
-                                match remainder, ss with
-                                | Unknown :: _, _ -> failwith "Did not expect an unkown character in here."
-                                | Spring :: _, _ -> false
-                                | Empty :: rs, ss -> validCombination memo ss rs
-                                | [], [] -> true
-                                | [], _ -> false
-
-        memo.Add(key, result)
-        result
-
-let solve records =
-    let allCombos =
-        records
-        |> List.map (fun (chars, sizes) -> (generateCombinations chars, sizes))
-
-    let memo = new Memo(10_000_000)
-
-    let validCombos =
-        allCombos
-        |> List.map (fun (combos, sizes) ->
-            (combos
-             |> List.filter (validCombination memo sizes)
-             |> List.length))
-        |> List.sum
-
-    validCombos
-
-let run () =
-    printf "Testing.."
-    test <@ 1 + 1 = 2 @>
-    printfn "...done!"
-
-run ()
-
 let replicate (chars, sizes) =
     let replicatedChars =
         chars :: List.replicate 4 (Unknown :: chars)
@@ -127,8 +44,42 @@ let replicate (chars, sizes) =
     let replicatedSizes = List.replicate 5 sizes |> List.collect id
     (replicatedChars, replicatedSizes)
 
+let rec countCombos (chars, sizes) = 
+    match sizes with
+    | [] -> 
+        if chars |> List.forall (fun c -> c = Empty || c = Unknown) then 1 else 0
+    | s :: ss ->
+        match chars with
+        | [] -> 0
+        | Empty :: cs -> countCombos (cs, sizes)
+        | Spring :: cs when cs |> List.length >= (s-1) ->
+            let (sub, rest) = cs |> List.splitAt (s - 1)
+            let allSprings = sub |> List.forall (fun c -> c = Spring || c = Unknown)
+            if allSprings then 
+                match rest with
+                | [] -> if ss |> List.isEmpty then 1 else 0
+                | Spring :: _ -> 0
+                | Empty :: rs -> countCombos (rs, ss)
+                | Unknown :: rs -> countCombos (rs, ss)
+            else 0
+        | Unknown :: cs ->
+            let asSpring = countCombos ((Spring :: cs),sizes)
+            let asEmpty = countCombos (cs,sizes)
+            asSpring + asEmpty
+        | _ -> 0
+
+let solve records =
+    records |> List.map countCombos |> List.sum
+
+let run () =
+    printf "Testing.."
+    test <@ 1 + 1 = 2 @>
+    printfn "...done!"
+
+run ()
+
 let records = input |> List.map parse
-//let replicated = records |> List.map replicate
+let replicated = records |> List.map replicate
 
 #time
-let result = records |> solve
+let result = replicated |> solve
