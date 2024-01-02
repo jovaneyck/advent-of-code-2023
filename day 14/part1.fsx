72dde162f81f23d899878c5b33b9c4acb8e120f9
Jo Van Eyck <j.vaneyck@farmad.be>
2024-01-02 09:57:31 +0100

    14.2

--- day 14/part1.fsx
+++ day 14/part1.fsx
@@ -26,18 +26,18 @@ module Array2D =
               for col in 0 .. (Array2D.length2 m - 1) do
                   if m[row, col] = v then (row, col) ]
 
+    let rotate (m: 'a [,]) =
+        let nbRows = Array2D.length1 m
+        let nbCols = Array2D.length2 m
+        let rotated row col = m[nbRows - 1 - col, row]
+        Array2D.init nbCols nbRows rotated
+
 type Cell =
     | Empty
     | Wall
     | Rock
 
 type Platform = Cell [,]
-
-type Direction =
-    | North
-    | East
-    | South
-    | West
 
 let parse =
     function
@@ -83,27 +83,59 @@ let tiltNorth platform =
     let rockLocations = platform |> Array2D.findIndices Rock
     rockLocations |> List.fold tiltRockNorth platform
 
+let cycle platform =
+    platform
+    |> tiltNorth
+    |> Array2D.rotate
+    |> tiltNorth
+    |> Array2D.rotate
+    |> tiltNorth
+    |> Array2D.rotate
+    |> tiltNorth
+    |> Array2D.rotate
+
+let rec findLoop visited n platform =
+    let next = cycle platform
+
+    match visited |> List.tryFind (fun (p, i) -> p = next) with
+    | Some (p, i) -> next, n - i, i
+    | None -> findLoop ((next, (n + 1)) :: visited) (n + 1) next
+
+let rec run n platform =
+    if n = 0 then
+        platform
+    else
+        run (n - 1) (cycle platform)
+
+let findLoad platform =
+    let rockCounts =
+        [ for row in 0 .. Array2D.length1 platform - 1 ->
+              platform[row, *]
+              |> Array.filter ((=) Rock)
+              |> Array.length ]
+
+    rockCounts
+    |> List.rev
+    |> List.mapi (fun i c -> i + 1, c)
+    |> List.map (fun (i, c) -> i * c)
+    |> List.sum
+
 let platform: Platform =
     input
     |> Seq.map (fun row -> row |> Seq.map (fun cell -> parse cell))
     |> array2D
 
 #time
-let tilted = platform |> tiltNorth
+//Using mutable array2D: Real: 00:00:52.462, CPU: 00:00:55.875, GC gen0: 425, gen1: 62, gen2: 2
+//Using array2D copies:  Real: 00:01:36.792, CPU: 00:01:42.250, GC gen0: 2208, gen1: 118, gen2: 1
+let (platformAfterLoop, loopLength, loopStart) =
+    findLoop [ (platform, 0) ] 0 platform
 
-let rockCounts =
-    [ for row in 0 .. Array2D.length1 tilted - 1 ->
-          tilted[row, *]
-          |> Array.filter ((=) Rock)
-          |> Array.length ]
+let cyclesToRun = (1_000_000_000 - loopStart) % (loopLength + 1)
+let finalPlatform = run cyclesToRun platformAfterLoop
+finalPlatform |> findLoad
 
-rockCounts
-|> List.rev
-|> List.mapi (fun i c -> i + 1, c)
-|> List.map (fun (i, c) -> i * c)
-|> List.sum
-
-let run () =
+let runT () =
     printf "Testing.."
 
     test
@@ -125,6 +157,15 @@ let run () =
 
            actual = expected @>
 
+    test
+        <@ let m = array2D [ [ 1; 2 ]; [ 3; 4 ] ]
+
+           let actual = m |> Array2D.rotate
+
+           let expected = array2D [ [ 3; 1 ]; [ 4; 2 ] ]
+
+           actual = expected @>
+
     printfn "...done!"
 
-run ()
+runT ()
