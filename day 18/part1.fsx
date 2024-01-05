97cf16f26793cffc68f516e0626e6906cb3157ed
Jo Van Eyck <j.vaneyck@farmad.be>
2024-01-05 16:38:08 +0100

    18 prep

---
+++ day 18/part1.fsx
@@ -0,0 +1,31 @@
+#r "nuget: Unquote"
+open Swensen.Unquote
+
+let input =
+    System.IO.File.ReadAllLines $"""{__SOURCE_DIRECTORY__}\input.txt"""
+    |> List.ofSeq
+
+let example =
+    """ R 6 (#70c710)
+        D 5 (#0dc571)
+        L 2 (#5713f0)
+        D 2 (#d2c081)
+        R 2 (#59c680)
+        D 2 (#411b91)
+        L 5 (#8ceee2)
+        U 2 (#caa173)
+        L 1 (#1b58a2)
+        U 2 (#caa171)
+        R 2 (#7807d2)
+        U 3 (#a77fa3)
+        L 2 (#015232)
+        U 2 (#7a21e3)""".Split("\n")
+    |> Array.map (fun s -> s.Trim())
+    |> List.ofSeq
+
+let run () =
+    printf "Testing.."
+    test <@ 1 + 1 = 2 @>
+    printfn "...done!"
+
+run ()
