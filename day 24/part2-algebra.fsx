87ed0c94a1ca5f9184dae133ccae90722113e803
Jo Van Eyck <j.vaneyck@farmad.be>
2024-01-28 19:20:28 +0100

    24.2 using a linear algebra package

--- day 24/part2-algebra.fsx
+++ day 24/part2-algebra.fsx
@@ -1,15 +1,4 @@
 #r "nuget: Unquote"
-#r "nuget: MathNet.Numerics.FSharp, 5.0.0"
-
-open MathNet.Numerics.LinearAlgebra
-
-let A =
-    matrix [ [ 3.0; 2.0; -1.0 ]
-             [ 2.0; -2.0; 4.0 ]
-             [ -1.0; 0.5; -1.0 ] ]
-
-let b = vector [ 1.0; -2.0; 0.0 ]
-let x = A.Solve(b) |> Seq.toList // 1;-2;-2
 
 open Swensen.Unquote
 open System.Text.RegularExpressions
@@ -28,122 +17,80 @@ let example =
     |> Array.map (fun s -> s.Trim())
     |> List.ofSeq
 
-type ThreeDimensionalCoordinate = decimal * decimal * decimal
-
-//y = ax + b
-type TwoDimensionalLine = { A: decimal; B: decimal }
-
-module TwoDimensionalLine =
-    let X y line = (y - line.B) / line.A
-    let Y x line = (line.A * x) + line.B
-
-    let intersection { A = a; B = b } { A = c; B = d } : ThreeDimensionalCoordinate option =
-        if (a - c) = 0m then
-            None
-        else
-            let x = (d - b) / (a - c)
-            let y = a * x + b
-            Some(x, y, -1m)
+type ThreeDimensionalCoordinate = double * double * double
 
 type HailStone =
     { Location: ThreeDimensionalCoordinate
       Velocity: ThreeDimensionalCoordinate }
 
-module HailStone =
-    let to2DLine (hs: HailStone) : TwoDimensionalLine =
-        let (x, y, _) = hs.Location
-        let (dx, dy, _) = hs.Velocity
-
-        let a = dy / dx
-        let b = y - a * x
-        { A = a; B = b }
-
-let between (min, max) value = min <= value && value <= max
-let inBox box (x, y, z) = x |> between box && y |> between box
-
 let parse (line: string) : HailStone =
     let re = Regex("(?<x>.*), (?<y>.*), (?<z>.*) @ (?<dx>.*), (?<dy>.*), (?<dz>.*)")
     let m = re.Match(line)
-    let get (key: string) = m.Groups[key].Value |> decimal
+    let get (key: string) = m.Groups[key].Value |> double
 
     { Location = (get "x", get "y", get "z")
       Velocity = (get "dx", get "dy", get "dz") }
 
-let combinations (xs: 'a list) =
-    let max = (xs |> List.length) - 1
-
-    [ for i in 0..max do
-          for j in (i + 1) .. max do
-              xs[i], xs[j] ]
-
-let run () =
-    printf "Testing.."
-
-    test
-        <@ parse "229429688799267, 202127443693857, 379401089168938 @ 63, 129, -113" = { Location =
-                                                                                             (229429688799267m,
-                                                                                              202127443693857m,
-                                                                                              379401089168938m)
-                                                                                         Velocity = (63m, 129m, -113m) } @>
-
-    test <@ combinations [ 1; 2; 3 ] = [ (1, 2); (1, 3); (2, 3) ] @>
-
-    let testIntersection one other =
-        let hone = (parse >> HailStone.to2DLine) one
-        let hother = (parse >> HailStone.to2DLine) other
-        let actual = TwoDimensionalLine.intersection hone hother
-        actual
-
-    let shouldEqual (expected: ThreeDimensionalCoordinate option) (actual: ThreeDimensionalCoordinate option) =
-        match actual, expected with
-        | None, None -> true
-        | Some (x1, y1, z1), Some (x2, y2, z2) ->
-            let eps = 0.001m
-
-            abs (x1 - x2) < eps
-            && abs (y1 - y2) < eps
-            && abs (z1 - z2) < eps
-        | _ -> false
-
-    test
-        <@ testIntersection "19, 13, 30 @ -2, 1, -2" "18, 19, 22 @ -1, -1, -2"
-           |> shouldEqual (Some(14.333m, 15.333m, -1m)) @>
-
-    test
-        <@ testIntersection "19, 13, 30 @ -2, 1, -2" "20, 25, 34 @ -2, -2, -4"
-           |> shouldEqual (Some(11.667m, 16.667m, -1m)) @>
-
-    test
-        <@ testIntersection "19, 13, 30 @ -2, 1, -2" "12, 31, 28 @ -1, -2, -1"
-           |> shouldEqual (Some(6.2m, 19.4m, -1m)) @>
-
-    test <@ inBox (7m, 27m) (14.333m, 15.333m, -1m) @>
-    test <@ inBox (7m, 27m) (11.667m, 16.667m, -1m) @>
-    test <@ inBox (7m, 27m) (6.2m, 19.4m, -1m) |> not @>
-    printfn "...done!"
-
-run ()
-
 let hailstones = input |> List.map parse
-let lines = hailstones |> List.map HailStone.to2DLine
-let both = List.zip hailstones lines
 
-let intersectionsInBox =
-    both
-    |> combinations
-    |> List.choose (fun ((hs1, l1), (hs2, l2)) ->
-        TwoDimensionalLine.intersection l1 l2
-        |> Option.map (fun intersection -> intersection, hs1, l1, hs2, l2))
-    |> List.filter
-        //Only "move forward in time" aka apply velocity positively
-        (fun ((x, y, z),
-              { Location = hs1x, hs1y, hs1z
-                Velocity = hs1dx, hs1dy, hs1dz },
-              _,
-              { Location = hs2x, hs2y, hs2z
-                Velocity = hs2dx, hs2dy, hs2dz },
-              _) ->
-            (x - hs1x) * hs1dx >= 0m
-            && (x - hs2x) * hs2dx >= 0m)
-    |> List.filter (fun (intersection, hs1, l1, hs2, l2) -> inBox (200000000000000m, 400000000000000m) intersection)
-    |> List.length
+(*
+rock: x,y,z @ dx,dy,dz
+hailstone_i = ai,bi,ci @ di,ei,fi
+equations:
+
+x,y: (e2 - e1)x + (d1 - d2)y    + 0z            + (b1 - b2)dx   + (a2 - a1) dy  + 0 dz          = a2e2 - b2d2 - a1e1 + b1d1
+x,z: (f2 - f1)x + 0y            + (d1 - d2)z    + (c1 - c2)dx   + 0 dy          + (a2 - a1) dz  = a2f2 - c2d2 - a1f1 + c1d1
+y,z: 0x         + (f2 - f1)y    + (e1 - e2)z    + 0 dx          + (c1 - c2)dy   + (b2 - b1) dz  = b2f2 - c2e2 - b1f1 + c1e1
+
+Use 2 different sets of hailstones (h1,h2) to get 6 equations in 6 variables
+Solve A*x=b
+
+*)
+
+let generateEquations (h1: HailStone) (h2: HailStone) =
+    let { Location = a1, b1, c1
+          Velocity = d1, e1, f1 } =
+        h1
+
+    let { Location = a2, b2, c2
+          Velocity = d2, e2, f2 } =
+        h2
+
+    let A =
+        [ [ (e2 - e1)
+            (d1 - d2)
+            0.0
+            (b1 - b2)
+            (a2 - a1)
+            0.0 ]
+          [ (f2 - f1)
+            0.0
+            (d1 - d2)
+            (c1 - c2)
+            0.0
+            (a2 - a1) ]
+          [ 0.0
+            (f2 - f1)
+            (e1 - e2)
+            0.0
+            (c1 - c2)
+            (b2 - b1) ] ]
+
+    let b =
+        [ a2 * e2 - b2 * d2 - a1 * e1 + b1 * d1
+          a2 * f2 - c2 * d2 - a1 * f1 + c1 * d1
+          b2 * f2 - c2 * e2 - b1 * f1 + c1 * e1 ]
+
+    A, b
+
+let A1, b1 = generateEquations hailstones.[0] hailstones.[1]
+let A2, b2 = generateEquations hailstones.[1] hailstones.[2]
+
+#r "nuget: MathNet.Numerics.FSharp, 5.0.0"
+
+open MathNet.Numerics.LinearAlgebra
+let A = matrix (A1 @ A2)
+
+let b = vector (b1 @ b2)
+let (x :: y :: z :: _) = A.Solve(b) |> Seq.map decimal |> Seq.toList // 1;-2;-2
+let part2 = x + y + z
