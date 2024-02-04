c992052821a65e65ca1ca2d5ea3f824e16c5fd6a
Jo Van Eyck <j.vaneyck@farmad.be>
2024-02-04 10:43:26 +0100

    added some dot and some svg artifacts

--- day 25/part1.fsx
+++ day 25/part1.fsx
@@ -18,19 +18,23 @@ let example =
         nvd: lhk
         lsr: lhk
         rzs: qnr cmg lsr rsh
-        frs: qnr lhk lsr""".Split("\n")
-            |> Array.map (fun s -> s.Trim())
-            |> List.ofSeq
+        frs: qnr lhk lsr"""
+        .Split("\n")
+    |> Array.map (fun s -> s.Trim())
+    |> List.ofSeq
 
-input 
-|> List.collect (fun line -> 
-                            let [|l;r|] = line.Split(": ")
-                            let rs = r.Split(" ") |> List.ofArray
-                            rs |> List.map (fun r -> sprintf "%s -> %s" l r))
+input
+|> List.collect (fun line ->
+    let [| l; r |] = line.Split(": ")
+    let rs = r.Split(" ") |> List.ofArray
+    rs |> List.map (fun r -> sprintf "%s -> %s" l r))
 |> Seq.iter (printfn "%s")
 //https://www.devtoolsdaily.com/graphviz
 //https://boxy-svg.com/
-let result = 706*717
+let totalNodes = 1423
+let left = 717
+let right = totalNodes - left
+let result = left * right //506202
 
 let run () =
     printf "Testing.."
